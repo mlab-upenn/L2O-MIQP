3c331127d9d25a6990509c29ebdbd6457aa14999
vietanhle0101 <51708449+vietanhle0101@users.noreply.github.com>
2025-11-04 14:17:51 -0500

    minor changes to load params from a yaml file

--- robot_nav/test.py
+++ robot_nav/test.py
@@ -1,10 +1,10 @@
 import torch
 import numpy as np
-
+import argparse
 import datetime
 import pickle
 import os
-
+import yaml
 from src.neural_net import *
 from trainer import *
 from cvxpy_mpc_layer import *
@@ -147,6 +147,26 @@ def make_cplayer(weights=None, M=None, H=None, bounds=None, T=None, obstacles=No
     return cplayer.to(torch.device("cpu")), meta
 
 def main():
+    cfg = yaml.safe_load(open("train_config.yaml"))
+    weights = cfg["weights"]
+    obj_weight = float(weights["w_obj"])
+    slack_weight = float(weights["w_slack"])
+    constraint_weight = float(weights["w_con"])
+    supervised_weight = float(weights["w_sup"])
+    print(f"Using loss weights - Obj: {obj_weight}, Slack: {slack_weight}, Constraint: {constraint_weight}, Supervised: {supervised_weight}")
+    loss_weights = [obj_weight, slack_weight, constraint_weight, supervised_weight]
+
+    training_params = cfg["training"]
+    training_params['TRAINING_EPOCHS'] = int(training_params['TRAINING_EPOCHS'])
+    training_params['CHECKPOINT_AFTER'] = int(training_params['CHECKPOINT_AFTER'])
+    training_params['LEARNING_RATE'] = float(training_params['LEARNING_RATE'])
+    training_params['WEIGHT_DECAY'] =  float(training_params['WEIGHT_DECAY'])
+    training_params['PATIENCE'] = int(training_params['TRAINING_EPOCHS'])
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%Y%m%d_%H%M%S")
+    training_params['RUN_NAME'] = "ssl_" + dt_string
+
+
     train_loader, test_loader, n_features, n_y, n_obs, Obs_info = prepare_data()
 
     cplayer, meta = make_cplayer()
@@ -169,23 +189,7 @@ def main():
 
     Model_SSL = SSL_MIQP_incorporated(nn_model_1, cplayer, 6, 4, device=device)
     # Model_SL = SSL_MIQP_incorporated(nn_model_2, cplayer, 6, 4, device=device)
-
-    now = datetime.datetime.now()
-    dt_string = now.strftime("%Y%m%d_%H%M%S")
-    training_params = {}
-    training_params['TRAINING_EPOCHS'] = int(10)
-    training_params['CHECKPOINT_AFTER'] = int(50)
-    training_params['LEARNING_RATE'] = 1e-3
-    training_params['WEIGHT_DECAY'] = 1e-5
-    training_params['PATIENCE'] = 5
-    training_params['WANDB_PROJECT'] = "l2o_ssl_miqp_robot_nav"
-    training_params['RUN_NAME'] = "ssl_" + dt_string
-
-    slack_weight = 0.0
-    constraint_weight = 1e3
-    supervised_weight = 1e5
-    loss_weights = [0.0, slack_weight, constraint_weight, supervised_weight]
-
+    
     Model_SSL.train_SSL(
         train_loader=train_loader, 
         test_loader=test_loader, 
